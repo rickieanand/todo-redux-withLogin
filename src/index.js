ca16d8c77e7e5afc7fda83b71d207ee8494a47a9
Rickie Anand <rickieanand@Apples-MacBook-Pro-2.local>
2019-03-20 02:00:20 +0530

    add browser-router to the site

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 // Required for Redux store setup
 import { Provider } from 'react-redux'
 import configureStore from './Store';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './index.css';
 import App from './App';
+import LoginContainer from './login/LoginContainer'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <Provider store={configureStore()}>
-      <App />
+        <Router>
+          <main>
+            <Route exact path="/" component={LoginContainer} />
+            <Route path="/app" component={App} />
+          </main>
+        </Router>
     </Provider>,
     document.getElementById('root')
   );
